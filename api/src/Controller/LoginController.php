f5f2b5e3a1a7953bec64210ae67130eeb98d199e
monoranjan.das <monoranjan.das@covianalytics.com>
2023-12-28 00:00:28 +0600

    Token generating with fetch request in symfony twig

--- api/src/Controller/LoginController.php
+++ api/src/Controller/LoginController.php
@@ -2,24 +2,18 @@
 
 namespace App\Controller;
 
-use App\Entity\User;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class LoginController extends AbstractController
 {
-    #[Route('/api/login', name: 'app_login')]
-    public function index(EntityManagerInterface $entityManager): JsonResponse
+    #[Route('/api/login', name: 'login')]
+    public function index(): Response
     {
-//        $user = new User();
-//        $user->setEmail('mono@gmail.com')->setRoles(['ROLE_USER'])->setPassword('123456');
-//        $entityManager->persist($user);
-        $entityManager->flush();
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/LoginController.php',
+        return $this->render('login/index.html.twig', [
+            'controller_name' => 'LoginController',
         ]);
     }
 }
