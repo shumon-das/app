69389110f130a8a2602636c49f9f3258c6c344b5
monoranjan.das <monoranjan.das@covianalytics.com>
2024-01-02 00:47:28 +0600

    installed reset password bundle

--- api/config/bundles.php
+++ api/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
